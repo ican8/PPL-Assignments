ebb0a60b8f898ef3b9f659b4781a787fe57d0bf0
soureesh <soureeshpatil@gmail.com>
2020-05-28 00:05:02 +0530

    added assignment 2

--- Assignment-2 (Stack Smashing)/overflow.c
+++ Assignment-2 (Stack Smashing)/overflow.c
@@ -1,25 +1,28 @@
 #include<stdio.h>
-
+#include<unistd.h>
 void fun();
-
+void exploit();
 int main(){
+    
+    printf("In main\n");
+    fun();
+    printf("Back to main\n");
+    return 0;   
+}
+
+void fun(){
     int a[5];
 
     for (int i = 0; i < 7; i++)
     {
         a[i] = i;
     }
-    a[7] = (int)&fun;
-    a[8] = (int)&fun;
-    a[9] = (int)&fun;
-    a[10] = (int)&fun;
-    a[11] = (int)&fun;
-   // a[12] = (int)&fun;
-    return 0;   
+    a[10] = (int)&exploit;
 }
 
-void fun(){
-    printf("You are hacked!\n");
-    int a[2];
-    a[4] = (int)&main;
+void exploit(void){
+	char *args[2];
+     args[0] = "/bin/sh";
+     args[1] = NULL;
+     execve(args[0], args, NULL);
 }
